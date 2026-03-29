e35914589d14d98b35e6d26d9b1de167b72cbaf6
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-03-29 12:18:23 +0530

    update

---
+++ src/HashSetl.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+public class HashSetl {
+
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        hs.add(15);
+        hs.add(20);
+        hs.add(20);
+        hs.add(70);
+        System.out.println(hs);
+    }
+}
