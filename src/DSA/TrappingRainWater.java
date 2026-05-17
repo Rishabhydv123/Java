d3017122c13cc5f002a94c0e05e83ff5c83da176
Rishabh Yadav <rishabhyadav55888@gmail.com>
2026-05-17 09:33:18 +0530

    Update TrappingRainWater.java

--- src/DSA/TrappingRainWater.java
+++ src/DSA/TrappingRainWater.java
@@ -28,8 +28,10 @@ public class TrappingRainWater {
         return water;
     }
 
+
     public static void main(String[] args) {
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println("Trapped Water: " + trap(height));
+        
+System.out.println("Trapped Water: " + trap(height));
     }
 }
